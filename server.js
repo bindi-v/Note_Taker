8318bc8e6d0660ebc3f356fbe79d87b4f66d7fca
bindi-v <bindi.vaghela@gmail.com>
2021-10-14 19:11:03 -0400

    added server.js file

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const htmlRoutes = require('./routes/home_routes');
+const apiRoutes = require('./routes/api_routes');
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
