395e5dab6f6046c4d7ee540b5e0ad3f212c7ce60
bindi-v <bindi.vaghela@gmail.com>
2021-10-16 14:22:36 -0400

    fix server.js and added Procfile to make heroku deploy url work

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const htmlRoutes = require('./routes/home_routes');
 const apiRoutes = require('./routes/api_routes');
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
